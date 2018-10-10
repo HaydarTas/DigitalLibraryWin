41b5c26acdee6b9b71c995624a7d78514dec1b4b
HaydarTas <haydartas1982@gmail.com>
2018-10-10 16:01:28 +0200

    test
    test

--- DigitalLibraryWin/Controllers/HomeController.cs
+++ DigitalLibraryWin/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace DigitalLibraryWin.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your contact pageTest.";
 
             return View();
         }

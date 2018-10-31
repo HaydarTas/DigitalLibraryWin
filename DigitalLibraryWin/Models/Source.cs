75cf84bc38905bce1f6273d9ede06332ce7b3e2e
Umais Usman Shoaib <u_09@live.dk>
2018-10-31 14:17:48 +0100

    Tilføjet Source og SourceController

---
+++ DigitalLibraryWin/Models/Source.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DigitalLibraryWin.Models
+{
+    public class Source
+    {
+        public String Title { get; set; }
+        public String Author { get; set; }
+        public String Language { get; set; }
+        public int Length { get; set; }
+        public String Format { get; set; }
+        public Array Tags { get; set; }
+    }
+}
